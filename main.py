859acc8c532ea3a89aa7e2dafb4ec1f9fd227ae0
mulchus <mulch@list.ru>
2023-08-26 22:04:38 +0300

    Added files reading

--- main.py
+++ main.py
@@ -1,48 +1,39 @@
 import os
 import argparse
 
-from environs import Env
+from dotenv import load_dotenv
 
 
 def main():
-    award_parser = argparse.ArgumentParser(description='Приложение "Викторина"')
-    award_parser.add_argument(
-        'path',
+    # load_dotenv()
+    # bot = telegram.Bot(os.environ.get('TELEGRAM_TOKEN'))
+
+    parser = argparse.ArgumentParser(description='Приложение "Викторина"')
+    parser.add_argument(
+        '--path',
         nargs='?',
-        default=os.path.join(os.getcwd(), 'awards.xlsx'),
-        help='директория с файлом awards.xlsx или awards_for_protocol.xlsx '
-             '(по умолчанию - ПУТЬ_К_ПАПКЕ_СО_СКРИПТОМ/awards.xlsx)'
+        default=os.path.join(os.getcwd(), 'quiz-questions'),
+        help='имя папки в корне проекта с файлами вопросов-ответов '
+             '(по умолчанию - quiz-questions)'
     )
-    award_parser.add_argument(
-        'task_id',
-        nargs='?',
-        default=1,
-        help='Тип задачи: \n'
-             '1 - Создание Почетных граммот и Благодарностей\n'
-             '2 - Создание выписок из протоколов\n'
+    parser.add_argument(
+        '--files',
+        nargs='*',
+        default='questions-answers.txt',
+        help='имена файлов вопросов-ответов для викторины '
+             '(по умолчанию - questions-answers.txt)'
     )
 
-    awards = {}
-    path = award_parser.parse_args().path
-    try:
-        awards = get_awards_from_file(path)
-    except (FileNotFoundError, ValueError) as error:
-        print(f'Неверно указан путь к файлу.\nОшибка: {error}')
-        print(f'Поиск в файле setup.txt в корневой папке.\n')
-        environs = Env()
+    path = os.path.join(os.getcwd(), parser.parse_args().path)
+
+    for file in parser.parse_args().files:
         try:
-            environs.read_env("setup.txt", recurse=False)
-            path = environs.str('PATH_TO_AWARDS_FILE')
-            awards = get_awards_from_file(path)
+            with open(os.path.join(path, file), "r", encoding="KOI8-R") as my_file:
+                file_contents = my_file.read()
+                # print(f'{file}: {file_contents[:50]}' )
         except (FileNotFoundError, ValueError) as error:
-            exit(f'setup.txt в корневой папке не найден или в нем не указан путь к файлу награждений.\n'
-                 f'Ошибка: {error}')
+            print(f'Неверно указан путь к файлу.\nОшибка: {error}')
 
-    print(f'Скрипт запущен с файлом данных {path}')
-
-
-
-    with open("myfile.txt", "r", encoding="ваша кодировка") as my_file:
 
 
 if __name__=='__main__':
